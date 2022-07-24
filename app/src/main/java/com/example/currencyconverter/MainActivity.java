606e7c9823b9f47b03303ed01f59af3a68cd9ed1
allan8araujo <allanzao08@gmail.com>
2022-07-24 00:27:58 -0300

    [UPDATE] add business logic

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -7,33 +7,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.currencyconverter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
         EditText etValorBrl = binding.etValue;
         Button buttonCalculate = binding.buttonCalculate;
         buttonCalculate.setOnClickListener(this);
-        TextView textDolar = binding.textDolar;
-        textDolar.setText("");
-        TextView textEuro = binding.textEuro;
-        textEuro.setText("");
+        this.clearValues();
 
         etValorBrl.getText();
-
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == findViewById(R.id.button_calculate).getId()){
-            // TODO implementar função de logica de conversão
+        Toast.makeText(this, "Informe um valor!", Toast.LENGTH_SHORT).show();
+
+        if (view.getId() == R.id.button_calculate){
+           String value = binding.etValue.getText().toString();
+           if (!value.isEmpty()){
+               Double real = Double.valueOf(value);
+               binding.textDolar.setText(String.format("%.2f", real/5.5));
+               binding.textEuro.setText(String.format("%.2f", real/5.61));
+           } else{
+               Toast.makeText(this, "Informe um valor!", Toast.LENGTH_SHORT).show();
+           }
         }
+    }
+
+    private void clearValues(){
+        binding.textDolar.setText("");
+        binding.textEuro.setText("");
     }
 }
