70ff38086cc647c9d0c9ce53d42965bf6f90d53d
allan8araujo <allanzao08@gmail.com>
2022-07-23 23:59:53 -0300

    [UPDATE] some changes

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.currencyconverter.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ActivityMainBinding binding;
 
     @Override
@@ -18,7 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         EditText etValorBrl = binding.etValue;
         Button buttonCalculate = binding.buttonCalculate;
+        buttonCalculate.setOnClickListener(this);
+        TextView textDolar = binding.textDolar;
+        textDolar.setText("");
+        TextView textEuro = binding.textEuro;
+        textEuro.setText("");
+
         etValorBrl.getText();
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == findViewById(R.id.button_calculate).getId()){
+            // TODO implementar função de logica de conversão
+        }
     }
 }
