1d1ba0a153ddaf2c95ad6654152d422b05c179c4
allan8araujo <allanzao08@gmail.com>
2022-07-23 23:48:10 -0300

    [NEW] Add view binding

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.currencyconverter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        EditText etValorBrl = binding.etValue;
+        Button buttonCalculate = binding.buttonCalculate;
+        etValorBrl.getText();
+
     }
 }
